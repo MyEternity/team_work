2acdd5b21f5e643f7d743afaa1d550415f59a283
dark <spddarkforce47@gmail.com>
2022-10-07 12:24:55 +0300

    users models

--- team_work/settings.py
+++ team_work/settings.py
@@ -143,3 +143,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 SUMMERNOTE_THEME = 'bs5'
 # Smart selects
 JQUERY_URL = True
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/users/login'
+LOGIN_ERROR_URL = '/'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(null=False, unique=True, db_index=True)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+
+
+class UserProfile(models.Model):
+    MALE = 'М'
+    FEMALE = 'Ж'
+    HIDDEN = 'НД'
+
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+        (HIDDEN, 'НД')
+    )
+
+    userid = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    birthday = models.DateField(verbose_name='Дата рождения', null=False)
+    about = models.TextField(verbose_name='О себе', blank=True, null=True)
+    gender = models.CharField(verbose_name='Пол', choices=GENDER_CHOICES, blank=True, max_length=5)
+    phone_number = models.CharField(max_length=16)
+    avatar_image = models.ImageField(upload_to='users_avatar', blank=True)
+    profile_image = models.ImageField(upload_to='users_photo', blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
