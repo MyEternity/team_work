c0ea7fe1ee9ad478b27d0faa8ff14700a16419c7
dark <spddarkforce47@gmail.com>
2022-10-07 12:51:26 +0300

    mvp branch

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Article
 
-# Register your models here.
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('article_body',)
+
+
+admin.site.register(Article, PostAdmin)
