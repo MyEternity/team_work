296f49e3fa42a1577181ad8afbcd36db509cf61e
araym51 <araimo@yandex.ru>
2022-10-09 11:44:04 +0300

    views.py - CRUD для статей

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import FormView
+from django.views.generic import FormView, CreateView, UpdateView, DetailView, TemplateView, DeleteView
+from .forms import ArticleAddUpdateDeleteForm
+from .models import Article
 
 
 # Create your views here.
@@ -12,3 +15,28 @@ class IndexView(FormView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         return context
+
+
+class CreatePostView(CreateView):
+    """
+    заготовка для проверки работы форм
+    """
+    model = Article
+    title = 'Добавить пост'
+    form_class = ArticleAddUpdateDeleteForm
+    template_name = 'articles/add_post.html'
+
+
+class UpdatePostView(UpdateView):
+    model = Article
+    title = 'Редактировать пост'
+    form_class = ArticleAddUpdateDeleteForm
+    template_name = 'articles/add_post.html'
+
+
+# удаление нужно?
+class DeletePostView(DeleteView):
+    model = Article
+    title = 'Редактировать пост'
+    form_class = ArticleAddUpdateDeleteForm
+    template_name = 'articles/add_post.html'
