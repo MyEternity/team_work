d821e62762710c15cebe4c2b1b201c3fca22d001
MyEternity <spddarkforce47@gmail.com>
2022-10-10 16:22:26 +0300

    fixed registration forms updated authorization forms

--- users/forms.py
+++ users/forms.py
@@ -5,8 +5,15 @@ from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Имя пользователя'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}))
+    username = forms.CharField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Имя пользователя'
+        self.fields['password'].widget.attrs['placeholder'] = 'Пароль'
+        self.fields['username'].label = 'Пользователь'
+        self.fields['password'].label = 'Пароль'
 
     class Meta:
         model = User
@@ -16,9 +23,20 @@ class UserLoginForm(AuthenticationForm):
 class UserRegistrationForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Имя пользователя'}))
     email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Повторите пароль'}))
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Придумайте логин'
+        self.fields['username'].label = 'Пользователь'
+        self.fields['email'].widget.attrs['placeholder'] = 'myemail@mail.ml'
+        self.fields['email'].label = 'Электронная почта'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Придумайте пароль'
+        self.fields['password1'].label = 'Пароль'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Повторите пароль'
+        self.fields['password2'].label = 'Повторите пароль'
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
-
+        fields = ('username', 'email', 'password1', 'password2')
