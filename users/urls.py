6f7c34f5673883bb829404d44424a5851ec0d01e
kenassash <kenas.sash@gmail.com>
2022-10-10 15:25:41 +0900

    Task_6

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import IndexView, AuthorizationView, RegistrationView
+from .views import IndexView, RegisterUser, LoginUser, logout_user
 
 app_name = 'users'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('authorization', AuthorizationView.as_view(), name='authorization'),
-    path('registration', RegistrationView.as_view(), name='registration'),
+    path('authorization', LoginUser.as_view(), name='authorization'),
+    path('logout', logout_user, name='logout'),
+    path('registration', RegisterUser.as_view(), name='registration'),
 ]
