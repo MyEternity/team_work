8083ab582d80466ce1ba775daa4ff6dd0135c6fc
KirillovaV <victmanio@yandex.ru>
2022-10-16 01:26:37 +0300

    fixtures

---
+++ articles/management/commands/save_db.py
@@ -0,0 +1,49 @@
+import json
+from os import path
+
+from django.core.management.base import BaseCommand
+from articles.models import ArticleCategory, Article
+from users.models import User
+
+JSON_PATH = 'articles/json'
+
+
+def save_json(file_name, data):
+    with open(path.join(JSON_PATH, file_name + '.json'), 'w', encoding='UTF-8') as file:
+        json.dump(data, file, indent=2)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        categories = ArticleCategory.objects.all()
+        data = []
+        for category in categories:
+            data.append({'guid': category.guid,
+                         'category_name': category.category_name,
+                         'category_image': str(category.category_image),
+                         'is_active': category.is_active})
+
+        save_json('categories', data)
+
+        users = User.objects.all()
+        data = []
+        for user in users:
+            data.append({'id': user.id,
+                         'username': user.username,
+                         'password': user.password,
+                         'email': user.email,
+                         'is_superuser': user.is_superuser,
+                         'is_staff': user.is_staff})
+
+            save_json('users', data)
+
+        articles = Article.objects.all()
+        data = []
+        for article in articles:
+            data.append({'guid': article.guid,
+                         'author_id': article.author_id.id,
+                         'category_id': article.category_id.guid,
+                         'topic': article.topic,
+                         'article_body': article.article_body})
+
+            save_json('articles', data)
