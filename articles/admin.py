258b68eb3a531c44ea86a2f4285795d79434507a
dark <spddarkforce47@gmail.com>
2022-10-16 19:50:40 +0300

    fix possible duplicates on article categories

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 
-from .models import Article, ArticleHistory, ArticleCategory
+from .models import Article, ArticleHistory, Category, ArticleCategory
 
 
 class PostAdmin(SummernoteModelAdmin):
@@ -11,3 +11,4 @@ class PostAdmin(SummernoteModelAdmin):
 admin.site.register(Article, PostAdmin)
 admin.site.register(ArticleHistory)
 admin.site.register(ArticleCategory)
+admin.site.register(Category)
