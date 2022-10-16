32f08b9ccb8315478c443a88d2ba7eacd9715bb7
lev-s <sem.lev.a@yandex.ru>
2022-10-16 21:52:42 +0500

    added parser for article preview

--- articles/views.py
+++ articles/views.py
@@ -4,7 +4,6 @@ from django.views.generic import FormView, CreateView, UpdateView, DetailView, T
 
 from team_work.mixin import BaseClassContextMixin, UserLoginCheckMixin, UserIsAdminCheckMixin
 from .forms import ArticleAddUpdateDeleteForm
-from .filters import ArticleFilter
 from django.urls import reverse_lazy
 from bs4 import BeautifulSoup
 
@@ -13,53 +12,34 @@ from articles.models import Article
 
 class IndexListView(BaseClassContextMixin, ListView):
     """Класс IndexListView - для вывода статей на главной страницы."""
-
     paginate_by = 3
     model = Article
-    articles_filtered = None
     title = 'Крабр - Лучше, чем Хабр'
+    # Шаблона еще нет, делаю на базоый шаблон.
     template_name = 'articles/articles_list.html'
 
     def get_queryset(self):
-        img_height = '400px'
-
-        # Сортировка, сверху - новые
+        preview_p = None
+        # Сортировка - сверху новые.
         qs = Article.objects.all().prefetch_related('author_id'). \
             order_by('-articlehistory__record_date')
-
-        # Фильтрация по поиску
-        self.articles_filtered = ArticleFilter(self.request.GET, queryset=qs)
-
-        # Работа с preview
-        for article in self.articles_filtered.qs:
-            preview_p = ''
-            new_article_body = ''
-
+        # Парсинг в поисках первого тега и изображения
+        for article in qs:
             soup = BeautifulSoup(article.article_body, 'html.parser')
-
             preview_img = soup.img
-            # Стилизация изображения под ограничение высоты, центрирование
-            if preview_img:
-                preview_img['style'] = f'height: {img_height}; object-fit: scale-down; float: none;' \
-                                       f' display: block; margin-left: auto; margin-right: auto;'
-                new_article_body += str(preview_img)
-
-            # Поиск первого существенного абзаца
             p_lst = soup.find_all('p')
             for p in p_lst:
                 if p.text:
-                    if p.img:
-                        p.img.decompose()
                     preview_p = p
                     break
-            new_article_body += str(preview_p)
-
-            article.article_body = new_article_body
-        return self.articles_filtered.qs
+            if preview_img:
+                article.article_body = str(preview_img) + str(preview_p)
+            else:
+                article.article_body = str(preview_p)
+        return qs
 
     def get_context_data(self, **kwargs):
         context = super(IndexListView, self).get_context_data(**kwargs)
-        context["filter"] = self.articles_filtered
         return context
 
 
