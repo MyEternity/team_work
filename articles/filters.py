a78518dddd448c2de9a560bcff33fc1ed585b3ba
lev-s <sem.lev.a@yandex.ru>
2022-10-17 18:34:37 +0500

    added modal_search

--- articles/filters.py
+++ articles/filters.py
@@ -4,12 +4,19 @@ from django.db.models import Q
 
 
 class ArticleFilter(FilterSet):
-    search = CharFilter(method='custom_filter', label="Поиск по статьям:")
-
     class Meta:
         model = Article
-        fields = ['search']
+        fields = ['topic']
 
-    def custom_filter(self, queryset, name, value):
-        queryset = queryset.filter(Q(article_body__icontains=value) | Q(topic__icontains=value))
-        return queryset
+    topic = CharFilter(field_name='topic', lookup_expr='icontains', label='Поиск по названию статьи:')
+
+# class ArticleFilter(FilterSet):
+#     q = CharFilter(method='custom_filter', label="Поиск по статьям:")
+#
+#     class Meta:
+#         model = Article
+#         fields = ['q']
+#
+#     def custom_filter(self, queryset, name, value):
+#         queryset = queryset.filter(Q(topic__icontains=value), Q(article_body__icontains=value))
+#         return queryset
