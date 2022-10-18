bb81ac2cec820fb06386278388eada90998bcab9
lev-s <sem.lev.a@yandex.ru>
2022-10-18 14:32:20 +0500

    update modal search and set up pagination

--- articles/filters.py
+++ articles/filters.py
@@ -4,19 +4,12 @@ from django.db.models import Q
 
 
 class ArticleFilter(FilterSet):
+    search = CharFilter(method='custom_filter', label="Поиск по статьям:")
+
     class Meta:
         model = Article
-        fields = ['topic']
+        fields = ['search']
 
-    topic = CharFilter(field_name='topic', lookup_expr='icontains', label='Поиск по названию статьи:')
-
-# class ArticleFilter(FilterSet):
-#     q = CharFilter(method='custom_filter', label="Поиск по статьям:")
-#
-#     class Meta:
-#         model = Article
-#         fields = ['q']
-#
-#     def custom_filter(self, queryset, name, value):
-#         queryset = queryset.filter(Q(topic__icontains=value), Q(article_body__icontains=value))
-#         return queryset
+    def custom_filter(self, queryset, name, value):
+        queryset = queryset.filter(Q(article_body__icontains=value) | Q(topic__icontains=value))
+        return queryset
