3aa1edcd2a296139b61015999a846902cb32f84c
dark <spddarkforce47@gmail.com>
2022-10-20 15:11:02 +0300

    updated comments count + likes count for article template via templatetag

--- articles/templatetags/article_tags.py
+++ articles/templatetags/article_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.template.defaultfilters import stringfilter
+
+from articles.models import Comment, ArticleLike
 
 register = template.Library()
 
@@ -10,3 +13,15 @@ def rm_obs_pages(data):
         data.pop('page')
         data._mutable = False
     return data.urlencode()
+
+
+@register.filter
+@stringfilter
+def get_comments_count(article_guid):
+    return str(Comment.count(article_guid))
+
+
+@register.filter
+@stringfilter
+def get_likes_count(article_guid):
+    return str(ArticleLike.count(article_guid))
