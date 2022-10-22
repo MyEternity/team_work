fd7d929b9ae397ff6257476579ab81ade8316383
KirillovaV <victmanio@yandex.ru>
2022-10-22 18:54:37 +0300

    fix active menu links

--- team_work/views.py
+++ team_work/views.py
@@ -3,7 +3,10 @@
 """
 from django.shortcuts import render
 
+from articles.models import Category
+
 
 # фунцкия для обработки ошибки 404 page not found
 def page_not_found_view(request, exception):
-    return render(request, '404_not_found.html', status=404)
+    return render(request, '404_not_found.html', status=404,
+                  context={'menu_links': Category.objects.all().order_by('name')})
