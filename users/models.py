00463f3bcdea1b60894d8dd2c109c80af712ec69
MyEternity <spddarkforce47@gmail.com>
2022-10-26 15:19:08 +0300

    update viewpost

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+import settings
 
 # Create your models here.
 class User(AbstractUser):
@@ -40,6 +40,14 @@ class UserProfile(models.Model):
         return f'{self.userid.username}, ' \
                f'email: {self.userid.email}, ' \
                f'создан: {self.creation_datetime}'
+
+    @staticmethod
+    def get_photo(user_id):
+        ref = UserProfile.objects.get(userid=user_id)
+        if ref:
+            return settings.MEDIA_URL + ref.profile_image.name if ref.profile_image else '/' + settings.STATIC_URL + 'images/comments-1.png'
+        else:
+            return '/' + settings.STATIC_URL + 'images/comments-1.png'
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
