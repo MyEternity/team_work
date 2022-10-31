4dbf5465fabb87441741211ed52ca7fd9d113fb0
Araym51 <araimo96@gmail.com>
2022-10-31 18:38:30 +0300

    функции для подсчета лайков у пользователя

--- users/rating_counter.py
+++ users/rating_counter.py
@@ -1,13 +1,32 @@
+from collections import Counter
+
 from django.db.models import Sum
 
-from articles.models import ArticleLike, Article
+from articles.models import ArticleLike, Article, Comment, CommentLike
 
 
-def count_rating(user_id):
-    counter = 0
-    articles = Article.objects.filter(author_id=user_id).order_by('guid')
-    for guid in articles:
-        counter = ArticleLike.objects.filter(article_uid_id=guid).aggregate(Sum('event_counter'))
-    print(counter)
+def count_articles_rating(user_id):
+    object_list = []
+    articles = Article.objects.filter(author_id=user_id)  # получаем guid всех статей автора
+    for guid in articles:  # собираем сумму все лайки ко всем статьям
+        object_list.append(ArticleLike.objects.filter(article_uid_id=guid).aggregate(Sum('event_counter')))
+    counter = Counter({'event_counter__sum': 0})
+    for value in object_list:  # считаем лайки
+        counter += Counter(value)
     return counter
 
+
+def count_comments_rating(user_id):
+    object_list = []
+    comments = Comment.objects.filter(user_id=user_id)  # получаем guid всех комментариев автора
+    for guid in comments:  # собираем сумму все лайки ко всем комментам
+        object_list.append(CommentLike.objects.filter(comment_uid=guid).aggregate(Sum('event_counter')))
+    counter = Counter({'event_counter__sum': 0})
+    for value in object_list:  # считаем лайки
+        counter += Counter(value)
+    return counter
+
+
+def total_rating(user_id):
+    total = count_articles_rating(user_id) + count_comments_rating(user_id)
+    return total
