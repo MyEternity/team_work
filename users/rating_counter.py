c2b3693f52aff78f139e7be1b28465cc9b711e2c
Araym51 <araimo96@gmail.com>
2022-10-31 21:17:31 +0300

    Функция total_rating заменена на user_rating. user_rating возвращает общее количество лайков у пользователя

--- users/rating_counter.py
+++ users/rating_counter.py
@@ -5,7 +5,30 @@ from django.db.models import Sum
 from articles.models import ArticleLike, Article, Comment, CommentLike
 
 
-def count_articles_rating(user_id):
+def user_rating(user_id):
+    # user_rating возвращает число соотвествующее общему количеству лайков
+    # которые были поставлены пользователю под статьями и комментариями
+    object_list = []
+    articles = Article.objects.filter(author_id=user_id)  # получаем guid всех статей автора
+
+    for guid in articles:  # собираем сумму все лайки ко всем статьям
+        object_list.append(ArticleLike.objects.filter(article_uid_id=guid).aggregate(Sum('event_counter')))
+
+    comments = Comment.objects.filter(user_id=user_id)  # получаем guid всех комментариев автора
+
+    for guid in comments:  # собираем сумму все лайки ко всем комментам
+        object_list.append(CommentLike.objects.filter(comment_uid=guid).aggregate(Sum('event_counter')))
+
+    counter = Counter({'event_counter__sum': 0})
+    for value in object_list:  # считаем лайки
+        counter += Counter(value)
+
+    return counter['event_counter__sum']
+
+
+# следующие три функции нигде не используются, но с их помощью можно добавить множители рейтинга для статей
+def count_articles_likes(user_id):
+    # функция считает лайки за статьи
     object_list = []
     articles = Article.objects.filter(author_id=user_id)  # получаем guid всех статей автора
     for guid in articles:  # собираем сумму все лайки ко всем статьям
@@ -16,7 +39,8 @@ def count_articles_rating(user_id):
     return counter
 
 
-def count_comments_rating(user_id):
+def count_comments_likes(user_id):
+    # функция считает лайки за комменты
     object_list = []
     comments = Comment.objects.filter(user_id=user_id)  # получаем guid всех комментариев автора
     for guid in comments:  # собираем сумму все лайки ко всем комментам
@@ -28,5 +52,7 @@ def count_comments_rating(user_id):
 
 
 def total_rating(user_id):
-    total = count_articles_rating(user_id) + count_comments_rating(user_id)
+    # функция складывает лайки за статьи и комментарии
+    # при необходимости тут можно добавлять множители
+    total = count_articles_likes(user_id) + count_comments_likes(user_id)
     return total['event_counter__sum']
