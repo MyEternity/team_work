bc3cd56b655b6b98b2df05a95bc12b287044733f
lev-s <sem.lev.a@yandex.ru>
2022-11-01 17:08:13 +0500

    added admin template and configured user model in admin

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import User, UserProfile
-from django_summernote.admin import SummernoteModelAdmin
+from django_summernote.admin import SummernoteInlineModelAdmin
 
 
-class InfoAdmin(SummernoteModelAdmin):
+class UserProfileInline(SummernoteInlineModelAdmin, admin.StackedInline):
+    model = UserProfile
     summernote_fields = ('about',)
 
 
-# Register your models here.
-admin.site.register(User)
-admin.site.register(UserProfile, InfoAdmin)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = (UserProfileInline, )
+    filter_horizontal = ['groups', 'user_permissions', ]
