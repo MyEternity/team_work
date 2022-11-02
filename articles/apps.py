f3bf957051274e205a1ab1f2d1c526df2aa0cc66
dark <spddarkforce47@gmail.com>
2022-11-02 19:18:50 +0300

    friendly names for admin panel

--- articles/apps.py
+++ articles/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ArticlesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'articles'
+    verbose_name = 'Статьи и публикации'
+
+
+class SummernoteConfig(AppConfig):
+    verbose_name = 'Вложения'
