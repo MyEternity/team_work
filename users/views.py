4c67a0f609ca89b943f1ec696238cbca04fb8898
Egor_Ostroumov <araimo@yandex.ru>
2022-11-05 12:11:41 +0300

    !!! Необходимо обновить миграции !!! team_work.settings - добавлены настройки для отправки email для подтверждения пользователя users.forms UserRegistrationForm - переопределен метод save users.models User - добавлены поля activation_key, activation_key_expires. Добавлен метод is_activation_key_expires. users.urls добавлен путь для проверки линка на активацию пользователя users.views RegistrationView - переопределен метод post, добавлены методы send_verify_user, verify

--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
-from django.contrib import messages
 # Create your views here.
+from django.contrib import messages, auth
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db import transaction
-from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, UpdateView
+from django.core.mail import send_mail
+from django.conf import settings
 
 from articles.models import Article
 from team_work.mixin import BaseClassContextMixin, UserLoginCheckMixin
@@ -30,6 +33,44 @@ class RegistrationView(BaseClassContextMixin, SuccessMessageMixin, CreateView):
     template_name = 'users/registration.html'
     success_url = reverse_lazy('users:authorization')
     success_message = 'You have successfully registered'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            if self.send_verify_user(user):
+                messages.set_level(request, messages.SUCCESS)
+                messages.success(request, 'Вы успешно зарегистрировались! На e-mail придет ссылка на активацию аккаунта.')
+                return HttpResponseRedirect(reverse('users:authorization'))
+            else:
+                messages.set_level(request, messages.ERROR)
+                messages.error(request, form.errors)
+        else:
+            messages.set_level(request, messages.ERROR)
+            messages.error(request, form.errors)
+        return render(request, self.template_name, {'form': form})
+
+    def send_verify_user(self, user):
+        # функция высылает сообщение пользователю на email с ссылкой на активацию аккаунта
+        verify_link = reverse('users:verify', args=[user.email, user.activation_key])
+        subject = f'Для активации учетной записи {user.username} пройдите по ссылке'
+        message = f'Для подверждения учетной записи {user.username} на портале {settings.DOMAIN_NAME} ' \
+                  f' пройдите по ссылке: \n http://127.0.0.1:8000{verify_link}'
+        return send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+    def verify(self, email, activate_key):
+        # функция после проверки email и ключа активации активирует аккаунт пользователя.
+        try:
+            user = User.objects.get(email=email)
+            if user and user.activation_key == activate_key and not user.is_activation_key_expires():
+                user.activation_key = ''
+                user.activation_key_expires = None
+                user.is_active = True
+                user.save()
+                auth.login(self, user)
+            return render(self, 'users/verification.html')  # здесь подумать, куда лучше перенаправить
+        except Exception as err:
+            return HttpResponseRedirect(reverse('articles:index'))
 
 
 # Профиль
