bf16a3e69351eb9d0ccd73469da1aa95be3da721
lev-s <sem.lev.a@yandex.ru>
2022-11-06 23:20:51 +0500

    fixed filters

--- articles/filters.py
+++ articles/filters.py
@@ -2,59 +2,42 @@ from django.db.models import Q, Sum
 from django.forms.widgets import TextInput, Select
 from django_filters import CharFilter, FilterSet, ChoiceFilter
 
-from .models import Article, ArticleLike
+from .models import Article
 
-DATE_CHOICES = (
+CHOICES = (
     ('new', 'Сначала новые'),
-    ('old', 'Сначала старые')
-)
-
-RATING_CHOICES = (
+    ('old', 'Сначала старые'),
     ('most', 'Сначала популярные'),
     ('least', 'Сначала непопулярные')
 )
 
 
-def get_likes_count(article_guid):
-    counter = ArticleLike.objects.filter(article_uid=article_guid).aggregate(Sum('event_counter')).get(
-        'event_counter__sum', 0)
-    return "0" if counter is None else str(counter)
-
-
 class ArticleFilter(FilterSet):
-    date = ChoiceFilter(choices=DATE_CHOICES, method='date_ordering_filter',
-                        label='По дате публикации:',
-                        widget=Select(attrs={'placeholder': 'Дата'}))
-
-    rating = ChoiceFilter(choices=RATING_CHOICES, method='rating_ordering_filter',
-                          label='По рейтингу:',
-                          widget=Select(attrs={'placeholder': 'Рейтинг'}))
-
     text = CharFilter(method='text_filter', label='', widget=TextInput(attrs={'placeholder': 'Текст'}))
+
+    ordering = ChoiceFilter(choices=CHOICES, method='ordering_filter',
+                            label='Сортировать по:',
+                            widget=Select(attrs={'placeholder': 'Сортировка'}))
 
     class Meta:
         model = Article
-        fields = ['rating', 'date', 'text']
-
-    def date_ordering_filter(self, queryset, name, value):
-        value = value
-        match value:
-            case 'new':
-                queryset = queryset.order_by('-creation_date')
-            case 'old':
-                queryset = queryset.order_by('creation_date')
-        return queryset
-
-    def rating_ordering_filter(self, queryset, name, value):
-        value = value
-        match value:
-            case 'most':
-                queryset = queryset.annotate(likes=Sum('articlelike__event_counter')).order_by('-likes')
-            case 'least':
-                queryset = queryset.annotate(likes=Sum('articlelike__event_counter')).order_by('likes')
-        return queryset
+        fields = ['ordering', 'text']
 
     def text_filter(self, queryset, name, value):
-        value = value
         queryset = queryset.filter(Q(article_body__icontains=value) | Q(topic__icontains=value))
+        return queryset
+
+    def ordering_filter(self, queryset, name, value):
+        field = ''
+        match value:
+            case 'new':
+                field = '-creation_date'
+            case 'old':
+                field = 'creation_date'
+            case 'most':
+                field = '-likes'
+            case 'least':
+                field = 'likes'
+        if field:
+            queryset = queryset.annotate(likes=Sum('articlelike__event_counter')).order_by(field)
         return queryset
